16b9a61e65111abc6bd992f8dcc1b3406fe188b6
wookyoungkim <viliketh1s98@naver.com>
2020-10-19 13:42:43 +0900

    동빈북 - 구현

--- 구현/4#8.py
+++ 구현/4#8.py
@@ -1,50 +1,30 @@
-def fixing(N, weak, dist, fixed, count, num):
-    #i번째 친구부터 체크하기
-    if 0 not in fixed.values():
-        return True
-    if num >= count:
-        return False
-    print(count, "명의 친구: ")
-    for w in weak:
-        if fixed[w] != 1:
-            #k부터 idx까지 전부 수리 완료 
-            idx = w + dist[num]
-            if idx > N:
-                for l in weak: 
-                    if l in range(0, idx%N+1):
-                        fixed[l] = 1
-                    elif l in range(w,N-1):
-                        fixed[l] = 1
-            else:
-                for l in weak:
-                    if l in range(w,idx+1):
-                        fixed[l] = 1
-            #print(num, ": ", w, "- ", fixed)
-            if fixing(N, weak, dist, fixed, count, num+1):
-                return True
-            if idx > N:
-                for l in weak: 
-                    if l in range(0, idx%N+1):
-                        fixed[l] = 0
-                    elif l in range(w,N-1):
-                        fixed[l] = 0
-            else:
-                for l in weak:
-                    if l in range(w,idx+1):
-                        fixed[l] = 0
-            
+import itertools 
 
 def solution(n, weak, dist):
     dist.sort(reverse=True)
-    fixed = {}
-    for w in weak:
-        fixed[w] = 0
+    length = len(weak)
 
-    for i in range(1, len(dist)+1):
-        #i명의 친구가 점검하는 경우
-        if fixing(n, weak, dist, fixed, i, 0):
-            return i
+    #길이 2배인 일자 리스트로 바꾸기
+    for i in range(length):
+        weak.append(weak[i]+n)
+    print(weak)
+    answer = len(dist)+1
 
-    return -1
+    #0부터 length-1까지 시작점
+    for start in range(length):
+        for friends in list(itertools.permutations(dist, len(dist))):
+            #투입되는 친구의 수
+            count = 1
+            #해당 친구가 점검하는 마지막 위치
+            position = weak[start] + friends[count-1]
 
-print(solution(12,	[1, 5, 6, 10],	[1, 2, 3, 4]))
+            for index in range(start, start+length):
+                if position < weak[index]:
+                    count += 1
+                    if count > len(dist):
+                        break
+                    position = weak[index] + friends[count-1]
+            answer = min(answer, count)
+    if answer > len(dist):
+        return -1
+    return answer
